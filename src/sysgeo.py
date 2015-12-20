a459f319cf1d2b28c75cab3818f66500964d552b
Ziyi Luo <zl2471@columbia.edu>
2015-12-19 20:23:44 -0500

    Renew sysgeo.py

--- src/sysgeo.py
+++ src/sysgeo.py
@@ -1,5 +1,7 @@
 from sympy.geometry import *
-from math import sqrt,pi
+from sympy.geometry.line import LinearEntity
+from sympy.geometry.entity import GeometryEntity
+from math import sqrt,pi,cos,sin
 
 PI = pi
 
@@ -18,16 +20,16 @@ class dot(Point):
 		return self.x
 	def getY(self):
 		return self.y
-	def setRunstep(self,pos,val):
-		if(pos == 'x'):
-			self.runStep[0]=val
-		elif(pos == 'y'):
-			self.runStep[1]=val
 	def getRunstep(self,pos):
 		if(pos == 'x'):
 			return self.runStep[0]
 		elif(pos == 'y'):
 			return self.runStep[1]
+	def setRunstep(self,pos,val):
+		if(pos == 'x'):
+			self.runStep[0]=val
+		elif(pos == 'y'):
+			self.runStep[1]=val
 	def renew(self,pos):
 		if(pos == 'x'):
 			self.x = self.x + self.runStep[0]
@@ -40,25 +42,43 @@ class line(object):
 	def __init__(self,a,b,end_point_1=None,end_point_2=None):
 		if(end_point_1 is None):
 			if(isinstance(a,Point)):
-				self.a = float(b.y - a.y)/(b.x-a.x)
-				self.b = float(a.y) - self.a * a.x
-				self.line_=Line(a,b)
+				if(b.x!=a.x):
+					self.a = float(b.y - a.y)/(b.x-a.x)
+					self.b = float(a.y) - self.a * a.x
+					self.line_=Line(a,b)
+				else:
+					self.a=float('inf')
+					self.b=float('inf')
+					self.line_=Line(a, b)
 			else:
 				self.a = float(a)
 				self.b = float(b)
-				self.line_=Line(Point(0,self.b), Point(1, self.a+self.b))
+				if(a!=float('inf')):
+					self.line_=Line(Point(0,self.b), Point(1, self.a+self.b))
+				else:
+					self.line_=Line(Point(0,0), Point(0,1))
 			self.isLine = True
 		else:
 			if(isinstance(a,Point)):
-				self.a = float(b.y - a.y) / (b.x - a.x)
-				self.b = float(a.y) - self.a * a.x
+				if(b.x!=a.x):
+					self.a = float(b.y - a.y) / (b.x - a.x)
+					self.b = float(a.y) - self.a * a.x
+					self.line_=Segment(Point(end_point_1, self.a * end_point_1 + self.b), Point(end_point_2, self.a * end_point_2 + self.b)) 
+				else:
+					self.a = float('inf')
+					self.b = float('inf')
+					self.line_=Segment(Point(a.x,a.y), Point(a.x,b.y))
 			else:
 				self.a = float(a)
 				self.b = float(b)
-			self.line_=Segment(Point(end_point_1, self.a * end_point_1 + self.b), Point(end_point_2, self.a * end_point_2 + self.b)) 
+				self.line_=Segment(Point(end_point_1, self.a * end_point_1 + self.b), Point(end_point_2, self.a * end_point_2 + self.b)) 
 			self.isLine = False
-			self.end_point_1=min(end_point_1,end_point_2)
-			self.end_point_2=max(end_point_1,end_point_2)
+			if(b.x!=a.x):
+				self.end_point_1=min(end_point_1,end_point_2)
+				self.end_point_2=max(end_point_1,end_point_2)
+			else:
+				self.end_point_1=a.x
+				self.end_point_2=a.x
 		self.runStep = [0,0,0]
 	def __str__(self):
 		if(self.isLine):
@@ -72,6 +92,31 @@ class line(object):
 			return self.a
 		elif (pos == 'b'):
 			return self.b
+	def setPara(self,pos,val):
+		if (pos == 'a'):
+			self.a = a
+		elif (pos == 'b'):
+			self.b = b
+		elif (pos == 'left_end'):
+			self.end_point_1 = val
+		elif (pos == 'right_end'):
+			self.end_point_2 = val
+		if (isLine):
+			self.line_=Line(Point(0,self.b), Point(1, self.a+self.b))
+		else:
+			self.line_=Segment(Point(self.end_point_1, self.a * self.end_point_1 + self.b), Point(self.end_point_2, self.a * self.end_point_2 + self.b))
+	def setparaYline(self,p1):
+		if(isinstance(p1,Point)):
+			self.line_=Line(Point(p1.x,0),Point(p1.x,1))
+			self.a=float('inf')
+			self.a=float('inf')
+	def setparaYsegm(self,p1,p2):
+		if(isinstance(p1,Point) and isinstance(p2,Point)):
+			self.line_=Segment(p1,p2)
+			self.a=float('inf')
+			self.a=float('inf')
+			self.end_point_1=min(p1.x,p2.x)
+			self.end_point_2=max(p1.x,p2.x)
 	def setRunstep(self,pos,val):
 		if(pos == 'a'):
 			self.runStep[0]=val
@@ -116,7 +161,10 @@ class line(object):
 		else:
 			return None
 	def contains(self,p):
-		return self.line_.contains(p)
+		if(isinstance(p,Point)):
+			return self.line_.contains(p)
+		else:
+			return None
 	def getMidpoint(self):	# Segment ONLY
 		if(self.isLine):
 			return dot(0,0)
@@ -149,11 +197,38 @@ class line(object):
 			return Line.is_parallel(self.line_, aline.line_)
 		return None
 	def intersect(self, geo_object):
-		if(isinstance(a,line.LinearEntity) or isinstance(a,Point)):
-			return self.line_.intersection(geo_object)
-		elif(isinstance(a,line)):
-			return self.line_.intersection(geo_object.line_)
-		return None
+		if(isinstance(geo_object,LinearEntity) or isinstance(geo_object,Point)):
+			tem = self.line_.intersection(geo_object)
+			if(len(tem)>0):
+				temp=[]
+				for i in tem:
+					if(isinstance(i,Point)):
+						temp.append(dot(i))
+					elif(isinstance(i,Line)):
+						t = i.points
+						temp.append(line(t[0],t[1]))
+					elif(isinstance(i,Segment)):
+						t = i.points
+						temp.append(line(t[0],t[1],t[0].x,t[1].x))
+				tem=temp
+			return tem
+		elif(isinstance(geo_object,line)):
+			tem = self.line_.intersection(geo_object.line_)
+			if(len(tem)>0):
+				temp=[]
+				for i in tem:
+					if(isinstance(i,Point)):
+						temp.append(dot(i))
+					elif(isinstance(i,Line)):
+						t = i.points
+						temp.append(line(t[0],t[1]))
+					elif(isinstance(i,Segment)):
+						t = i.points
+						temp.append(line(t[0],t[1],t[0].x,t[1].x))
+				tem=temp
+			return tem
+		else:
+			return None
 
 class circle(object):
 	def __init__(self,a,b):
@@ -169,7 +244,16 @@ class circle(object):
 	def getCenter(self):
 		return self.center
 	def getRadius(self):
-		return self.radius
+		return self
+	def setCenter(self,a,b=None):
+		if(isinstance(a,Point) and (b is None)):
+			self.center = a
+		else:
+			self.center=dot(a,b)
+		self.circle_=Circle(self.center,self.radius)
+	def setRadius(self,r):
+		self.radius=r
+		self.circle_=Circle(self.center,self.radius)
 	def setRunstep(self,pos,val):
 		if(pos == 'x'):
 			self.runStep[0]=val
@@ -177,6 +261,10 @@ class circle(object):
 			self.runStep[1]=val
 		elif(pos == 'r'):
 			self.runStep[2]=val
+	def getPointbyarc(self,arc):
+		finalx = float(self.center.x + self.radius * cos(arc))
+		finaly = float(self.center.y + self.radius * sin(arc))
+		return dot(finalx,finaly)
 	def getRunstep(self,pos):
 		if(pos == 'x'):
 			return self.runStep[0]
@@ -194,6 +282,32 @@ class circle(object):
 		elif(pos == 'r'):
 			self.radius = self.radius + self.runStep[2]
 		self.circle_ = Circle(self.center,self.radius)
+	def intersect(self, geo_object):
+		if(not (isinstance(geo_object,GeometryEntity) or isinstance(geo_object,line) or isinstance(geo_object,circle))):
+			return None
+		if(isinstance(geo_object,GeometryEntity)):
+			tem = self.circle_.intersection(geo_object)
+		elif(isinstance(geo_object,line)):
+			tem = self.circle_.intersection(geo_object.line_)
+		elif(isinstance(geo_object,circle)):
+			tem = self.circle_.intersection(geo_object.circle_)
+		if(isinstance(tem,Circle)):
+			return circle(tem.center,tem.radius)
+		if(len(tem)>0):
+			temp=[]
+			for i in tem:
+				if(isinstance(i,Point)):
+					temp.append(dot(i))
+				elif(isinstance(i,Line)):
+					t = i.points
+					temp.append(line(t[0],t[1]))
+				elif(isinstance(i,Segment)):
+					t = i.points
+					temp.append(line(t[0],t[1],t[0].x,t[1].x))
+				elif(isinstance(i,Circle)):
+					temp.append(circle(i.center,i.radius))
+			tem=temp
+		return tem
 
 class runset:
 	def __init__(self,runtime):
