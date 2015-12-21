4d5786565aa59e0099d3678159cf8b889cee8623
chaozc <zichen.chao@gmail.com>
2015-12-20 19:07:48 -0500

    hardcode removed & syslib updated

--- src/generate_hardcode.py
+++ src/generate_hardcode.py
@@ -1,12 +1,10 @@
-inf = open('hardcode', 'r')
+inf = open('geoSysFunc.lib', 'r')
 for line in inf:
 	line
 	paras = line.split(", ")
 	paras2 = paras[2][1:-2].split("+")
 	for i in range(len(paras2)):
 		paras2[i] = "\""+paras2[i]+"\""
-	#print paras2
-	
 	if paras2[0] == '""':
 		print "env := {vars = StringMap.empty; funcs = StringMap.add \""+paras[0]+"\" "+"\""+paras[1]+"\" env.contents.funcs; get_call = \"\"; func_opt = StringMap.add \""+paras[0]+"\" [] env.contents.func_opt};;"
 	else:
--- src/install.sh
+++ src/install.sh
@@ -1,2 +1,6 @@
+python generate_hardcode.py > geo_hardcode
+python install.py > compile_sc_py.ml
 sh compile_sc_py.sh
 chmod +x geo
+rm compile_sc_py.ml
+rm geo_hardcode
