f0fa1dd144ef6f1f2572a657aca2c3eec9bd1eee
Ziyi Luo <zl2471@columbia.edu>
2015-12-31 12:12:13 -0500

    Final

--- geo/tests/test-for.py
+++
@@ -1,7 +0,0 @@
-from Tkinter import *
-from sysgeo import *
-
-PI = 3.14159265359
-for i in [1, "zcCHAO", 2, "zyLUO", 3, "qWANG", 4, "ycZHAO", True, ';']:
-	print i
-
--- geo/tests/test-while.py
+++
@@ -1,9 +0,0 @@
-from Tkinter import *
-from sysgeo import *
-
-PI = 3.14159265359
-count = 0
-while ((count < 11)):
-	print count
-	count = (count + 2)
-
